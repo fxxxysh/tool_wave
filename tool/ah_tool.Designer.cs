5d745c15a7f69d11c77171b586cbe0aae1907232
fxxxysh <1208207637@qq.com>
2018-03-28 11:07:30 +0800

    创建示波器工具

---
+++ tool/ah_tool.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace tool
+{
+    partial class ah_tool
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Iocomp.Classes.PlotChannelTrace plotChannelTrace1 = new Iocomp.Classes.PlotChannelTrace();
+            Iocomp.Classes.PlotDataCursorXY plotDataCursorXY1 = new Iocomp.Classes.PlotDataCursorXY();
+            Iocomp.Classes.PlotDataView plotDataView1 = new Iocomp.Classes.PlotDataView();
+            Iocomp.Classes.PlotLabelBasic plotLabelBasic1 = new Iocomp.Classes.PlotLabelBasic();
+            Iocomp.Classes.PlotLegendBasic plotLegendBasic1 = new Iocomp.Classes.PlotLegendBasic();
+            Iocomp.Classes.PlotXAxis plotXAxis1 = new Iocomp.Classes.PlotXAxis();
+            Iocomp.Classes.PlotYAxis plotYAxis1 = new Iocomp.Classes.PlotYAxis();
+            this.plot1 = new Iocomp.Instrumentation.Plotting.Plot();
+            this.SuspendLayout();
+            // 
+            // plot1
+            // 
+            this.plot1.LoadingBegin();
+            plotChannelTrace1.Color = System.Drawing.Color.Red;
+            plotChannelTrace1.Name = "Channel 1";
+            plotChannelTrace1.TitleText = "Channel 1";
+            this.plot1.Channels.Add(plotChannelTrace1);
+            plotDataCursorXY1.Hint.Fill.Pen.Color = System.Drawing.SystemColors.InfoText;
+            plotDataCursorXY1.Name = "Data-Cursor 1";
+            plotDataCursorXY1.TitleText = "Data-Cursor 1";
+            this.plot1.DataCursors.Add(plotDataCursorXY1);
+            plotDataView1.Name = "Data-View 1";
+            plotDataView1.TitleText = "Data-View 1";
+            this.plot1.DataViews.Add(plotDataView1);
+            plotLabelBasic1.DockOrder = 0;
+            plotLabelBasic1.Name = "Label 1";
+            plotLabelBasic1.TitleText = "Label 1";
+            this.plot1.Labels.Add(plotLabelBasic1);
+            plotLegendBasic1.DockOrder = 0;
+            plotLegendBasic1.Name = "Legend 1";
+            plotLegendBasic1.TitleText = "Legend 1";
+            this.plot1.Legends.Add(plotLegendBasic1);
+            this.plot1.Location = new System.Drawing.Point(36, 80);
+            this.plot1.Name = "plot1";
+            this.plot1.Size = new System.Drawing.Size(975, 455);
+            this.plot1.TabIndex = 0;
+            plotXAxis1.DockOrder = 0;
+            plotXAxis1.Name = "X-Axis 1";
+            plotXAxis1.Title.Text = "X-Axis 1";
+            this.plot1.XAxes.Add(plotXAxis1);
+            plotYAxis1.DockOrder = 0;
+            plotYAxis1.Name = "Y-Axis 1";
+            plotYAxis1.Title.Text = "Y-Axis 1";
+            this.plot1.YAxes.Add(plotYAxis1);
+            this.plot1.LoadingEnd();
+            // 
+            // ah_tool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 583);
+            this.Controls.Add(this.plot1);
+            this.Name = "ah_tool";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Iocomp.Instrumentation.Plotting.Plot plot1;
+    }
+}
+
